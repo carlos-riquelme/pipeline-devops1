a08eb799f50f35456384a36c678105285cf7a34d
Carlos <carlos.riquelme@gmail.com>
2022-02-01 15:46:52 -0300

    agrega arrayExtensions 3

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -6,39 +6,39 @@
 
 def call(){
   
- pipeline {
-    agent any
-    environment {
-        NEXUS_USER = credentials('NEXUS-USER')
-        NEXUS_PASSWORD = credentials('NEXUS-PASS')
-    }
-    parameters {
-        choice choices: ['maven', 'gradle'], description: 'Seleccione una herramienta para preceder a compilar', name: 'compileTool'
-            text description: 'Enviar los stages separados por ";"... Vacío si necesita todos los stages', name: 'stages'
-    }
-    stages {
-        stage("Pipeline"){
-            steps {
-                  script{
-                      sh "env"
-                      env.TAREA = ""
-                      if(params.compileTool == 'maven'){
-                        maven.call(params.stages);
-                      }else{
-                        gradle.call(params.stages)
-                      }
-                  }
-              }
-            post{
-                success{
-                    slackSend color: 'good', message: "[Carlos  Riquelme] [${JOB_NAME}] [${BUILD_TAG}] Ejecucion Exitosa", teamDomain: 'dipdevopsusac-tr94431', tokenCredentialId: '09bb43a7-7b85-4368-8bcb-b6cc51f1010e'
-                }
-                failure{
-                    slackSend color: 'danger', message: "[Carlos Riquelme] [${env.JOB_NAME}] [${BUILD_TAG}] Ejecucion fallida en stage [${env.TAREA}]", teamDomain: 'dipdevopsusac-tr94431', tokenCredentialId: '09bb43a7-7b85-4368-8bcb-b6cc51f1010e'
+    pipeline {
+        agent any
+            environment {
+                NEXUS_USER = credentials('NEXUS-USER')
+                NEXUS_PASSWORD = credentials('NEXUS-PASS')
+            }
+            parameters {
+                choice choices: ['maven', 'gradle'], description: 'Seleccione una herramienta para preceder a compilar', name: 'compileTool'
+                text description: 'Enviar los stages separados por ";"... Vacío si necesita todos los stages', name: 'stages'
+            }
+            stages {
+                stage("Pipeline"){
+                    steps {
+                        script{
+                            sh "env"
+                            env.TAREA = ""
+                            if(params.compileTool == 'maven'){
+                                maven.call(params.stages);
+                            }else{
+                                gradle.call(params.stages)
+                            }
+                        }
+                    }
+                    post{
+                        success{
+                            slackSend color: 'good', message: "[Carlos  Riquelme] [${JOB_NAME}] [${BUILD_TAG}] Ejecucion Exitosa", teamDomain: 'dipdevopsusac-tr94431', tokenCredentialId: '09bb43a7-7b85-4368-8bcb-b6cc51f1010e'
+                        }
+                        failure{
+                            slackSend color: 'danger', message: "[Carlos Riquelme] [${env.JOB_NAME}] [${BUILD_TAG}] Ejecucion fallida en stage [${env.TAREA}]", teamDomain: 'dipdevopsusac-tr94431', tokenCredentialId: '09bb43a7-7b85-4368-8bcb-b6cc51f1010e'
+                        }
+                    }
                 }
             }
-        }
     }
-}
 }
 return this;
